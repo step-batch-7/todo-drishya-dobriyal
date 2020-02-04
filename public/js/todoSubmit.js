6209ab14195ab06ab0e48dacc61ac24d3fe8b789
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-02-04 10:05:16 +0530

    added delting feture while writing todo

--- public/js/todoSubmit.js
+++ public/js/todoSubmit.js
@@ -1,20 +1,28 @@
 const addItem = () => {
-  const textarea = document.createElement('textarea');
-  textarea.rows = '2';
-  textarea.cols = '105';
-  textarea.type = 'text';
-  document.getElementById('tasks').appendChild(textarea);
+  const div = document.createElement('div');
+  div.classList.add('item');
+  div.innerHTML = ` <textarea rows="2" cols="105" type="text"></textarea>
+   <div onclick="deleteItem()" id="deleteItem">-</div>`;
+  document.getElementById('tasks').appendChild(div);
 };
 
 const addTodo = () => {
   const title = document.getElementById('title').value;
+
   let items = Array.from(document.querySelectorAll('textarea'));
   items = items.map(item => item.value);
   const xhr = new XMLHttpRequest();
-  const postBody = `title=${title}&todoItems=${items.join('\r\n')}`
+  const postBody = `title=${title}&todoItems=${items.join('\r\n')}`;
+
   xhr.open('POST', '/saveTodo');
   xhr.send(postBody);
+
   document.getElementById('tasks').innerHTML = '';
   document.getElementById('title').value = '';
   addItem();
+};
+
+const deleteItem = () => {
+  const itemToDelete = event.target.parentNode;
+  itemToDelete.parentNode.removeChild(itemToDelete)
 };
