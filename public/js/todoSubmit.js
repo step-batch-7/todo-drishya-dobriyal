fee0b0391d34e37d9b58c5dc4502347a51f99257
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-02-04 16:20:15 +0530

    modified front end

--- public/js/todoSubmit.js
+++ public/js/todoSubmit.js
@@ -1,7 +1,7 @@
 const addItem = () => {
   const div = document.createElement('div');
   div.classList.add('item');
-  div.innerHTML = ` <textarea rows="2" cols="105" type="text"></textarea>
+  div.innerHTML = ` <textarea rows="2" cols="105" type="text" required></textarea>
    <div onclick="deleteItem()" id="deleteItem">-</div>`;
   document.getElementById('tasks').appendChild(div);
 };
@@ -20,9 +20,41 @@ const addTodo = () => {
   document.getElementById('tasks').innerHTML = '';
   document.getElementById('title').value = '';
   addItem();
+  displayTodo();
 };
 
 const deleteItem = () => {
   const itemToDelete = event.target.parentNode;
-  itemToDelete.parentNode.removeChild(itemToDelete)
+  itemToDelete.parentNode.removeChild(itemToDelete);
 };
+
+const getItemsHtml = items => {
+  return items.map(item => {
+    return `<li>${++item.id}: ${item.item}  ${item.taskStatus}</li>`;
+  }).join('<br>');
+};
+
+const getTemplate = list => {
+  let items = list.todoItems;
+  itemsHtml = getItemsHtml(items);
+  return `<div class="template">
+  <h2>${list.title}</h2>
+  <div>${itemsHtml}</div><br>
+  <h3 onclick='editTodo()' class="editTodo">Edit Todo</h3>
+  </div>`;
+};
+
+const displayTodo = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      const content = JSON.parse(xhr.responseText);
+      const template = content.map(getTemplate).join('');
+      document.getElementById('content').innerHTML = template;
+    };
+  };
+  xhr.open('GET', '/todoList.json');
+  xhr.send();
+};
+
+document.onload = displayTodo();
