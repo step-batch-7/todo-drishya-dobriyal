a0820909d8c26bd7caf295c0c45d8c64a1e22cfb
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-02-04 22:56:28 +0530

    modified the data structure

--- public/js/todoSubmit.js
+++ public/js/todoSubmit.js
@@ -2,25 +2,8 @@ const addItem = () => {
   const div = document.createElement('div');
   div.classList.add('item');
   div.innerHTML = ` <textarea rows="2" cols="105" type="text" required></textarea>
-   <div onclick="deleteItem()" id="deleteItem">-</div>`;
-  document.getElementById('tasks').appendChild(div);
-};
-
-const addTodo = () => {
-  const title = document.getElementById('title').value;
-
-  let items = Array.from(document.querySelectorAll('textarea'));
-  items = items.map(item => item.value);
-  const xhr = new XMLHttpRequest();
-  const postBody = `title=${title}&todoItems=${items.join('\r\n')}`;
-
-  xhr.open('POST', '/saveTodo');
-  xhr.send(postBody);
-
-  document.getElementById('tasks').innerHTML = '';
-  document.getElementById('title').value = '';
-  addItem();
-  displayTodo();
+      <div onclick="deleteItem()" id="deleteItem"> - </div>`;
+  event.target.parentNode.appendChild(div);
 };
 
 const deleteItem = () => {
@@ -28,33 +11,61 @@ const deleteItem = () => {
   itemToDelete.parentNode.removeChild(itemToDelete);
 };
 
-const getItemsHtml = items => {
-  return items.map(item => {
-    return `<li>${++item.id}: ${item.item}  ${item.taskStatus}</li>`;
-  }).join('<br>');
+const saveTodo = () => {
+  const title = document.getElementById('title').value;
+  let items = Array.from(document.querySelectorAll('textarea'));
+  const xhr = new XMLHttpRequest();
+  items = items.map(item => item.value);
+  const postBody = `title=${title}&todoItems=${items.join('\r\n')}`;
+  xhr.open('POST', '/saveTodo');
+  xhr.send(postBody);
+
+  document.getElementById('tasks').innerHTML =
+    ` <label>TITLE</label><br>
+      <input type="text" name="title" id="title" required></input>
+      <button onclick="addItem()">Add Item</button>
+      <button onclick="saveTodo()">DONE</button>`;
 };
 
-const getTemplate = list => {
-  let items = list.todoItems;
-  itemsHtml = getItemsHtml(items);
-  return `<div class="template">
-  <h2>${list.title}</h2>
-  <div>${itemsHtml}</div><br>
-  <h3 onclick='editTodo()' class="editTodo">Edit Todo</h3>
-  </div>`;
+const createTodoTemplate = (id, todo) => {
+  let todoTemplate = `<h2>${id}<h2>`;
+  todo.forEach(({ item }) => {
+    todoTemplate += `<li>${item}</li>`;
+  });
+  todoTemplate += ` <button onclick="editTodo()" id="${id}">EDIT TODO</button>`
+  return todoTemplate;
 };
 
 const displayTodo = () => {
+  const id = event.target.id;
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    const content = JSON.parse(xhr.responseText);
+    const todo = content[id];
+    const todoTemplate = createTodoTemplate(id, todo);
+
+    document.getElementById('content').innerHTML = todoTemplate;
+  };
+  xhr.open('GET', '/todoList.json');
+  xhr.send();
+};
+
+const createTodoListTemplate = (titleList) => {
+  return titleList.map(title => `<li id='${title}' onclick="displayTodo()">${title}</li>`).join('');
+};
+
+const displayTodoList = () => {
   const xhr = new XMLHttpRequest();
   xhr.onload = () => {
     if (xhr.status === 200) {
       const content = JSON.parse(xhr.responseText);
-      const template = content.map(getTemplate).join('');
-      document.getElementById('content').innerHTML = template;
+      let titleList = Object.keys(content);
+      titleList = createTodoListTemplate(titleList);
+      document.getElementById('todoList').innerHTML = titleList;
     };
   };
   xhr.open('GET', '/todoList.json');
   xhr.send();
 };
 
-document.onload = displayTodo();
+document.onload = displayTodoList();
