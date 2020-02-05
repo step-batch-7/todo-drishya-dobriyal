623556e0c3724a9508fac7387c49fce844952f4c
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-02-05 14:50:37 +0530

    added feature of deleting a saved todo

--- public/js/todoSubmit.js
+++ public/js/todoSubmit.js
@@ -8,7 +8,7 @@ const addItem = () => {
 
 const deleteItem = () => {
   const itemToDelete = event.target.parentNode;
-  itemToDelete.parentNode.removeChild(itemToDelete);
+  itemToDelete.remove();
 };
 
 const saveTodo = () => {
@@ -29,10 +29,10 @@ const saveTodo = () => {
 
 const createTodoTemplate = (id, todo) => {
   let todoTemplate = `<h2>${id}<h2>`;
-  todo.forEach(({ item }) => {
-    todoTemplate += `<li>${item}</li>`;
-  });
-  todoTemplate += ` <button onclick="editTodo()" id="${id}">EDIT TODO</button>`
+  for (const id in todo) {
+    todoTemplate += `<li>${todo[id]}</li>`
+  };
+  todoTemplate += `<button onclick="editTodo()" id="${id}">EDIT TODO</button>`
   return todoTemplate;
 };
 
@@ -60,12 +60,49 @@ const displayTodoList = () => {
     if (xhr.status === 200) {
       const content = JSON.parse(xhr.responseText);
       let titleList = Object.keys(content);
+      const heading = '<h3>TODO LIST\'s</h3>'
       titleList = createTodoListTemplate(titleList);
-      document.getElementById('todoList').innerHTML = titleList;
+      document.getElementById('content').innerHTML = heading + titleList;
     };
   };
   xhr.open('GET', '/todoList.json');
   xhr.send();
+};
+
+const saveEditedTodo = () => {
+  const parentNode = event.target.parentNode;
+  const xhr = new XMLHttpRequest();
+  const title = parentNode.querySelector('div input').value;
+  let items = Array.from(parentNode.querySelectorAll('div textarea'));
+  items = items.map(item => item.value);
+  const postBody = `title=${title}&todoItems=${items.join('\r\n')}`;
+  xhr.open('POST', '/saveTodo');
+  xhr.send(postBody);
+  displayTodoList();
+};
+
+const editTodo = () => {
+  const id = event.target.id;
+  const data = document.getElementById(id).parentElement;
+  let items = Array.from(data.querySelectorAll('li'));
+
+  items = items.map(item => {
+    return `<div> <textarea rows="2" cols="105" type="text" required>${item.textContent}</textarea>
+      <div onclick="deleteItem()" id="deleteItem"> - </div></div>`;
+  }).join('');
+  const headingTitle = `<input type="text" name="title" id=${id}></input>`;
+
+  document.getElementById('content').innerHTML =
+    `<div id="task">
+    <div id=${id}>
+    <label>TITLE</label><br>
+    ${headingTitle}
+    <button onclick="addItem()">Add Item</button>
+    <button onclick="saveEditedTodo()">DONE</button>
+    ${items}
+    </div>
+    </div>`;
+  document.getElementById('task').querySelector('input').value = id;
 };
 
 document.onload = displayTodoList();
