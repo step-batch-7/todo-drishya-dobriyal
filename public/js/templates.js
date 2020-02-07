0ed914d817a1dbc701dee6a17ab4478e5c867026
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-02-07 18:11:11 +0530

    modified templates

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,15 +1,3 @@
-const defaultCheckBox = () => `<input type="checkBox" onclick="toggleStatus"></input>`;
-
-const createNewItemTemplate = ({ id, statusCode, item }) => {
-  statusCode ? statusCode = 'checked' : statusCode = '';
-  const li = document.createElement('li');
-  li.setAttribute('id', id);
-  li.innerHTML = `<input type="checkBox" onclick="toggleStatus()" ${statusCode}>
-               ${item}
-               <div onclick="deleteItem()" class="deleteItem" style='display: flex; justify-content: space-evenly'> - </div>`;
-  return li;
-};
-
 const createItemTemplate = ({ id, statusCode, item }) => {
   statusCode ? statusCode = 'checked' : statusCode = '';
   return `<li id='${id}'>
@@ -29,22 +17,32 @@ const createTodoTemplate = (todo) => {
   return todoTemplate;
 };
 
-const newItemTemplate = () => {
-  const div = document.createElement('div');
-  div.classList.add('newItem');
-  div.innerHTML =
-    `${defaultCheckBox()}
-      <textarea rows="2" cols="105" type="text" required></textarea>
-      <div onclick="saveNewItem()" class="newItem"> + </div>
-      <div onclick="deleteItem()" class="deleteItem"> - </div>`;
-  return div
-};
-
 const showTitleTemplate = () => {
   return `<label> TITLE</label> <br>
     <input type="text" name="title" id="title" required></input>
     <button onclick="saveTitle()">DONE</button>`
 }
+
+const createNewItemTemplate = ({ id, statusCode, item }) => {
+  statusCode ? statusCode = 'checked' : statusCode = '';
+  const li = document.createElement('li');
+  li.setAttribute('id', id);
+  li.innerHTML = `<input type="checkBox" onclick="toggleStatus()" ${statusCode}>
+               ${item}
+               <div onclick="deleteItem()" class="deleteItem" style='display: flex; justify-content: space-evenly'> - </div>`;
+  return li;
+};
+
+const newItemTemplate = () => {
+  const div = document.createElement('div');
+  div.classList.add('newItem');
+  div.innerHTML =
+    `<input type="checkBox" onclick="toggleStatus"></input>
+      <textarea rows="2" cols="105" type="text" required></textarea>
+      <div onclick="saveNewItem()" class="newItem"> + </div>
+      <div onclick="deleteItem()" class="deleteItem"> - </div>`;
+  return div;
+};
 
 const displayNewTitle = (id, title) => {
   const li = document.createElement('li');
