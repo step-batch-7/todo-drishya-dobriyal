e0c999b138c9d743c98d48a64ca76f95d0c15920
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-02-08 15:16:23 +0530

    added test for post method

--- test/handlerTest.js
+++ test/handlerTest.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 const { DATA_STORE } = require('../config');
 const { app } = require('../lib/assignHandlers');
 
+const dataStore = fs.readFileSync(DATA_STORE, 'utf8');
+
 describe('GET method', function () {
   it('for home page for / url', function (done) {
     request(app.serve.bind(app))
@@ -25,8 +27,67 @@ describe('GET method', function () {
       .expect('Content-Type', /application\/javascript/)
       .expect(200, done);
   });
+  it('for /todoData', function (done) {
+    request(app.serve.bind(app))
+      .get('/todoList.json')
+      .set('Accept', '*/*')
+      .expect('Content-Type', /application\/json/)
+      .expect(200, done);
+  });
 });
 
-describe("POST method",function(){
-  
+describe("POST saveData", function () {
+  after(() => {
+    fs.writeFileSync(DATA_STORE, dataStore);
+  })
+  it('should save title on /saveTitle Req', function (done) {
+    request(app.serve.bind(app))
+      .post('/saveTitle')
+      .set('Accept', '*/*')
+      .send('{"title":"Bat"}')
+      .expect(200, done)
+      .expect('Content-Type', 'application/json')
+  });
+  it('should save item on /saveItem Req', function (done) {
+    request(app.serve.bind(app))
+      .post('/saveItem')
+      .set('Accept', '*/*')
+      .send('{ "task": { "item": "new Item", "statusCode": "false" }, "id":"T_1" }')
+      .expect(200, done)
+      .expect('Content-Type', 'application/json')
+  });
+  it('should delete item on /deleteItem Req', function (done) {
+    request(app.serve.bind(app))
+      .post('/deleteItem')
+      .set('Accept', '*/*')
+      .send('{ "itemId": "I_1", "titleId":"T_1" }')
+      .expect(200, done)
+      .expect('Content-Type', 'application/json')
+  });
+  it('should delete item on /deleteTitle Req', function (done) {
+    request(app.serve.bind(app))
+      .post('/deleteTitle')
+      .set('Accept', '*/*')
+      .send('{ "titleId":"T_1" }')
+      .expect(200, done)
+      .expect('Content-Type', 'application/json')
+  });
+  it('should delete item on /toggleStatus Req', function (done) {
+    request(app.serve.bind(app))
+      .post('/toggleStatus')
+      .set('Accept', '*/*')
+      .send('{ "itemId": "I_3", "titleId":"T_2" }')
+      .expect(200, done)
+      .expect('Content-Type', 'application/json')
+  });
+});
+
+describe("method not Handled", function () {
+  it("should give method not allowed for unhandled method", function (done) {
+    request(app.serve.bind(app))
+      .put('/saveTitle')
+      .set('Accept', '*/*')
+      .send('{"title":"Bat"}')
+      .expect(400, done)
+  })
 });
