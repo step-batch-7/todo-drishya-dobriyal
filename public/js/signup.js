13bd7630eda50794db7afa2c79c04f18ed773087
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-02-15 12:18:56 +0530

    [drishya | anuja] Added userExists mechanism to signUp
    now while signUp on each keypress added mechanism
    that will tell if that usrname is availabel or not

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,10 +1,23 @@
-const signUp = function () {
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
-  const callback = () => console.log(username, password);
-  const postBody = JSON.stringify({ username, password });
-  sendNewRequest('POST', '/signUp', postBody, callback);
-};
+
+const checkUsername = function (event) {
+  const username = event.target.value;
+  const callback = (response) => {
+    const { userExists } = JSON.parse(response);
+    if (userExists) {
+      document.getElementById('username').style.outlineColor = 'red';
+      document.getElementById('existingStatus').innerText = "user already exists"
+      document.getElementById('signup').disabled = true;
+    }
+    else {
+      document.getElementById('username').style.outlineColor = 'cornflowerBlue';
+      document.getElementById('existingStatus').innerText = '';
+      document.getElementById('signup').disabled = false;
+
+    }
+  }
+  const data = JSON.stringify({ username })
+  sendNewRequest('POST', '/checkUserNameAvailability', data, callback)
+}
 
 const sendNewRequest = function (method, url, data, callback) {
   const xhr = new XMLHttpRequest();
@@ -12,5 +25,6 @@ const sendNewRequest = function (method, url, data, callback) {
     callback(xhr.responseText);
   };
   xhr.open(method, url);
+  xhr.setRequestHeader('Content-Type', 'application/json')
   xhr.send(data);
 };
